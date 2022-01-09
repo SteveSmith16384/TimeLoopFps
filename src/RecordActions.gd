7bb861ae4b7c0b5f93334aef2999beb4a0f9644f
= <stephen.carlylesmith@googlemail.com>
2022-01-09 14:54:00 +0000

    Fixed rewinding

--- src/RecordActions.gd
+++ src/RecordActions.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var actions = []
-var start_time : int
+var start_time_global : int
 var pointer : int  = 0
 var mode # : Globals.RecMode
-var end_time : int
+#var end_time_diff : int
 
 func _ready():
 	pass
@@ -20,10 +20,10 @@ func start(_mode):
 		$Timer_StorePos.wait_time = 0.05
 	elif mode == Globals.RecMode.Rewinding:
 		pointer = actions.size()-1
-		end_time = actions[pointer].time
-		$Timer_StorePos.wait_time = 0.001
+#		end_time_diff = actions[pointer].time
+#		$Timer_StorePos.wait_time = 0.001
 		
-	start_time = OS.get_ticks_msec()
+	start_time_global = OS.get_ticks_msec()
 	$Timer_StorePos.start() # todo - adjust interval depending on mode
 	pass
 	
@@ -33,7 +33,7 @@ func _on_Timer_StorePos_timeout():
 		var data = {
 			type = Globals.RecType.Movement,
 			pos = self.get_parent().translation,
-			time = OS.get_ticks_msec() - start_time,
+			time = OS.get_ticks_msec() - start_time_global,
 			rot = self.get_parent().rotation
 		}
 		actions.push_back(data)
@@ -44,14 +44,18 @@ func _on_Timer_StorePos_timeout():
 			
 		var peek = actions[pointer]
 		if peek:
-			var time = end_time - (OS.get_ticks_msec() - start_time)
-			if peek.time >= time/10:
+			var total_duration = Globals.PHASE_DURATION * 1000
+			var so_far = OS.get_ticks_msec() - start_time_global
+			var time = total_duration - (so_far * 5)
+			print("Time:" + str(time))
+			while peek.time >= time and pointer > 0:
 				if peek.type == Globals.RecType.Movement:
 					get_parent().translation = peek.pos
 					get_parent().rotation = peek.rot
 #				elif peek.type == Globals.RecType.Shoot:
 #					get_parent().shoot()
 				pointer -= 1
+				peek = actions[pointer]
 	elif mode == Globals.RecMode.Playing:
 		if pointer >= actions.size():
 			$Timer_StorePos.stop()
@@ -59,7 +63,7 @@ func _on_Timer_StorePos_timeout():
 			
 		var peek = actions[pointer]
 		if peek:
-			var time = OS.get_ticks_msec() - start_time
+			var time = OS.get_ticks_msec() - start_time_global
 			if peek.time <= time:
 				if peek.type == Globals.RecType.Movement:
 					get_parent().translation = peek.pos
@@ -73,7 +77,7 @@ func _on_Timer_StorePos_timeout():
 func add_shot():
 	var data = {
 		type = Globals.RecType.Shoot,
-		time = OS.get_ticks_msec() - start_time,
+		time = OS.get_ticks_msec() - start_time_global,
 	}
 	actions.push_back(data)
 	pass
